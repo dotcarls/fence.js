423b042292b647c121aaecee7ebabeb287b69ad3
Tim Carlson <tim.carlson@velocloud.net>
2017-12-08 21:00:56 -0800

    convert to method syntax

--- src/lib/Result.js
+++ src/lib/Result.js
@@ -1,99 +1,102 @@
 /**
- * Adapter for mapping validation invokables to their results
- *
- * @param    {Array}    invokables    Array of invokable validation functions
- * @param    {Array}    results       Array of values returned from invoked validation functions
- */
-function Result (invokables, results, subject) {
-    this._invokables = invokables;
-    this._results = results;
-    this._subject = subject;
-}
+* Adapter for mapping validation invokables to their results
+*/
+class Result {
+    /**
+    * @param    {Array}    invokables    Array of invokable validation functions
+    * @param    {Array}    results       Array of values returned from invoked validation functions
+    */
+    constructor (invokables, results, subject) {
+        this._invokables = invokables;
+        this._results = results;
+        this._subject = subject;
+    }
 
-/**
- * A Results summary that will return `true` if all validations passed
- *
- * @return    {Boolean}    `true` if all Results are also `true`, `false` otherwise
- */
-Result.prototype.forAll = function() {
-    const results = this._results;
+    /**
+    * A Results summary that will return `true` if all validations passed
+    *
+    * @return    {Boolean}    `true` if all Results are also `true`, `false` otherwise
+    */
+    forAll () {
+        const results = this._results;
 
-    return results.reduce(function(acc, result) {
-        if (Array.isArray(result)) {
-            return acc && result.reduce(function(subAcc, subResult) {
-                return subAcc && subResult.forAll();
-            }, acc);
-        }
+        return results.reduce(function(acc, result) {
+            if (Array.isArray(result)) {
+                return acc && result.reduce(function(subAcc, subResult) {
+                    return subAcc && subResult.forAll();
+                }, acc);
+            }
 
-        return acc && result === true;
-    }, true);
-};
+            return acc && result === true;
+        }, true);
+    }
 
-/**
- * A Results summary that will return `true` if any validations passed
- * @return    {Boolean}    `true` if any one Result is also `true`, `false` if
- *                         all results are also `false`
- */
-Result.prototype.forAny = function() {
-    const results = this._results;
+    /**
+    * A Results summary that will return `true` if any validations passed
+    * @return    {Boolean}    `true` if any one Result is also `true`, `false` if
+    *                         all results are also `false`
+    */
+    forAny () {
+        const results = this._results;
 
-    return results.reduce(function (acc, result) {
-        if (Array.isArray(result)) {
-            return acc || result.reduce(function(subAcc, subResult) {
-                return subAcc || subResult.forAny();
-            }, acc) === true;
-        }
+        return results.reduce(function (acc, result) {
+            if (Array.isArray(result)) {
+                return acc || result.reduce(function(subAcc, subResult) {
+                    return subAcc || subResult.forAny();
+                }, acc) === true;
+            }
 
-        return acc || result === true;
-    }, false);
-};
+            return acc || result === true;
+        }, false);
+    }
 
-/**
- * Filters results by name
- * @param     {String}    name    The name for a specific Invokable. This value
- *                                is set by `ValidationBuilder.register`
- * @return    {Array}             An array of Booleans derived from specified
- *                                Invokables
- */
-Result.prototype.forOne = function(name) {
-    const invokables = this._invokables;
-    const results = this._results;
+    /**
+    * Filters results by name
+    * @param     {String}    name    The name for a specific Invokable. This value
+    *                                is set by `ValidationBuilder.register`
+    * @return    {Array}             An array of Booleans derived from specified
+    *                                Invokables
+    */
+    forOne (name) {
+        const invokables = this._invokables;
+        const results = this._results;
 
-    return results.filter(function(element, index) {
-        return invokables[index].getName() === name;
-    });
-};
+        return results.filter(function(element, index) {
+            return invokables[index].getName() === name;
+        });
+    }
 
-Result.prototype.explain = function(logger, indent) {
-    logger = logger || console.log;
-    indent = indent || '  ';
+    explain (logger, indent) {
+        logger = logger || console.log;
+        indent = indent || '  ';
 
-    logger(indent, 'subject:', JSON.stringify(this._subject));
-    logger(indent + indent, this.forAll() ? '[✓]' : '[x]', 'forAll');
-    logger(indent + indent, this.forAny() ? '[✓]' : '[x]', 'forAny');
+        logger(indent, 'subject:', JSON.stringify(this._subject));
+        logger(indent + indent, this.forAll() ? '[✓]' : '[x]', 'forAll');
+        logger(indent + indent, this.forAny() ? '[✓]' : '[x]', 'forAny');
 
-    logger(indent, 'tests:');
-    for (var i = 0; i < this._results.length; i++) {
-        const result = this._results[i];
-        const invokable = this._invokables[i];
-        const testName = invokable._name;
-        const testArgs = invokable._args;
+        logger(indent, 'tests:');
+        for (var i = 0; i < this._results.length; i++) {
+            const result = this._results[i];
+            const invokable = this._invokables[i];
+            const testName = invokable._name;
+            const testArgs = invokable._args;
 
-        let testLabel = testName;
-        if (testArgs.length > 0) {
-            testLabel += ' (' + JSON.stringify(testArgs) + ')';
-        }
+            let testLabel = testName;
+            if (testArgs.length > 0) {
+                testLabel += ' (' + JSON.stringify(testArgs) + ')';
+            }
 
-        logger(indent + indent, result ? '[✓]' : '[x]', testLabel);
+            logger(indent + indent, result ? '[✓]' : '[x]', testLabel);
 
-        if (Array.isArray(result)) {
-            result.forEach(function(subResult) {
-                subResult.explain(logger, indent + indent);
-            });
+            if (Array.isArray(result)) {
+                result.forEach(function(subResult) {
+                    subResult.explain(logger, indent + indent);
+                });
 
-            continue;
+                continue;
+            }
         }
     }
-};
+}
 
 export default Result;
--- src/lib/Validation.js
+++ src/lib/Validation.js
@@ -1,29 +1,32 @@
 import Result from './Result';
 
 /**
- * A 'built' ValidationBuilder that can be exported for use in other functions.
- *
- * @param    {array}    invokables    Array of invokable functions
- */
-function Validation (invokables) {
-    this._invokables = invokables;
+* A 'built' ValidationBuilder that can be exported for use in other functions.
+*/
+class Validation {
+    /**
+    * @param    {array}    invokables    Array of invokable functions
+    */
+    constructor (invokables) {
+        this._invokables = invokables;
+    }
+
+    /**
+    * Iterates over an array of invokables. Each invokable is called with a subject
+    * which is the attribute value being validated.
+    *
+    * @param     {any}         subject     Some value to be validated
+    * @return    {Result}                  An object representing invoked functions
+    *                                      and their outputs
+    */
+    run (...subjects) {
+        const invokables = this._invokables;
+        const results = invokables.map(function(invokable) {
+            return invokable.invoke.apply(invokable, subjects);
+        });
+
+        return new Result(invokables, results, subjects);
+    }
 }
-
-/**
- * Iterates over an array of invokables. Each invokable is called with a subject
- * which is the attribute value being validated.
- *
- * @param     {any}         subject     Some value to be validated
- * @return    {Result}                  An object representing invoked functions
- *                                      and their outputs
- */
-Validation.prototype.run = function(...subjects) {
-    const invokables = this._invokables;
-    const results = invokables.map(function(invokable) {
-        return invokable.invoke.apply(invokable, subjects);
-    });
-
-    return new Result(invokables, results, subjects);
-};
 
 export default Validation;
