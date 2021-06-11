8206fa099e1ab60110f89f623abd98b506304676
Tim Carlson <tim.carlson@gmail.com>
2021-06-10 22:59:57 -0700

    Update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import babel from 'rollup-plugin-babel';
 import nodeResolve from 'rollup-plugin-node-resolve';
-import uglify from 'rollup-plugin-uglify';
+// import uglify from 'rollup-plugin-uglify';
 import filesize from 'rollup-plugin-filesize';
 import commonjs from 'rollup-plugin-commonjs';
 
@@ -19,7 +19,7 @@ const plugins = [
 ];
 
 const isProd = process.env.NODE_ENV === `production`;
-if (isProd) plugins.push(uglify());
+// if (isProd) plugins.push(uglify());
 
 export default {
     input: `src/index.js`,
